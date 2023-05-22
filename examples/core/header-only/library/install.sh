3b95de62ba78d72481f0ce3e29e0cbf1d70b0605
oox <oiotoxt@gmail.com>
2023-05-22 11:29:29 +0900

    ~

--- examples/core/header-only/library/install.sh
+++ examples/core/header-only/library/install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 cmake --build build --target install
+
+# https://stackoverflow.com/questions/48428647/how-to-use-cmake-to-install
+# https://cmake.org/cmake/help/v3.20/manual/cmake.1.html#build-tool-mode
+# cmake --install build # >= cmake 3.15
